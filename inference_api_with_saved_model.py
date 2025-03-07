2c274d0b74ed619840fecf5fb3fd6a84dabc6c50
Aditya Dwi Nugraha <100182583+Adityadn64@users.noreply.github.com>
2025-03-07 08:05:49 +0700

    Update inference_api_with_saved_model.py

--- inference_api_with_saved_model.py
+++ inference_api_with_saved_model.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, render_template
+from flask_ngrok import run_with_ngrok
 import tensorflow as tf
 import numpy as np
 from PIL import Image
@@ -59,4 +60,5 @@ def predict():
     except Exception as e: return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run()
+    run_with_ngrok(app)
